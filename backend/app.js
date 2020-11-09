e31e598c393a505cb643698352dcae41a85dbcb5
Ayricko <aymeric_doucet@hotmail.com>
2020-11-09 12:34:05 +0100

    Add security function :punch:

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,14 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
-const cors = require('cors');
+const helmet = require('helmet');
+require('dotenv').config();
+
+//constante de connection BDD
+const mdbUser = process.env.DB_USER;
+const mdbPass = process.env.DB_PASS;
+const mdbAdress = process.env.DB_ADRESS;
+const mdbData = process.env.DB_DATA;
 
 // Importation des router
 const userRoutes = require('./routes/user');
@@ -12,12 +19,14 @@ const sauceRoutes = require('./routes/sauce');
 
 // Connection a MongoDB
 mongoose
-  .connect('mongodb+srv://Ayricko:8YAVASGnX2Ks1myE@cluster0.pt9lz.mongodb.net/P6OCR?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(`mongodb+srv://${mdbUser}:${mdbPass}@${mdbAdress}/${mdbData}`, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+// Sécurise les entête HTTP et empeche les attaques XSS, etc
+app.use(helmet());
+
 // Eviter les problème lié a CORS
-app.use(cors());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -19,13 +19,13 @@ exports.likeSauce = (req, res, next) => {
   const likeStatus = req.body.like;
   // Si L'utilisateur like la sauce
   if (likeStatus === 1) {
-    // On utilise le modele mongoose updateOne sur le modèle Sauce en passant en premier paramètre l'id de la sauce concernée, puis on incremente les likes de cette sauce d'une unité, et on ajoute l'id de l'utilisateur au tableau userLiked.
+    // On utilise le modele mongoose updateOne sur le modèle Sauce en passant en premier paramètre l'id de la sauce concernée, puis on incremente les likes de cette sauce de 1, et on ajoute l'id de l'utilisateur au tableau usersLiked.
     Sauce.updateOne({ _id: req.params.id }, { $inc: { likes: 1 }, $push: { usersLiked: req.body.userId } })
       .then(() => res.status(200).json({ message: 'Like ajouté' }))
       .catch((error) => res.status(400).json({ error }));
     // Sinon si l'utilisateur dislike
   } else if (likeStatus === -1) {
-    // On utilise la methode mongoose updateOne sur le modèle Sauce en passant en premier paramètre l'id de la sauce concernée, puis on incremente les dislikes de cette sauce d'une unité, et on ajoute l'id de l'utilisateur au tableau userDisliked.
+    // On utilise la methode mongoose updateOne sur le modèle Sauce en passant en premier paramètre l'id de la sauce concernée, puis on incremente les dislikes de cette sauce d'une unité, et on ajoute l'id de l'utilisateur au tableau usersDisliked.
     Sauce.updateOne({ _id: req.params.id }, { $inc: { dislikes: 1 }, $push: { usersDisliked: req.body.userId } })
       .then(() => res.status(200).json({ message: 'Dislike ajouté' }))
       .catch((error) => res.status(400).json({ error }));
@@ -36,13 +36,13 @@ exports.likeSauce = (req, res, next) => {
       .then((sauce) => {
         // Si l'utilisateur est dans le tableau des likes
         if (sauce.usersLiked.includes(req.body.userId)) {
-          // Alors on enleve une unitée de like dans la sauce concernée, et on retire l'utilisateur du tableau des userLiked
+          // Alors on enleve une unité de like dans la sauce concernée, et on retire l'utilisateur du tableau des usersLiked
           Sauce.updateOne({ _id: req.params.id }, { $inc: { likes: -1 }, $pull: { usersLiked: req.body.userId } })
             .then((sauce) => res.status(200).json({ message: 'Déselectionné !' }))
             .catch((error) => res.status(400).json({ error }));
           // Sinon si l'utilisateur est dans le tableau des dislikes
         } else if (sauce.usersDisliked.includes(req.body.userId)) {
-          // Alors on enleve une unitée de like dans la sauce concernée, et on retire l'utilisateur du tableau des userLiked
+          // Alors on enleve une unité de like dans la sauce concernée, et on retire l'utilisateur du tableau des usersDisliked
           Sauce.updateOne({ _id: req.params.id }, { $inc: { dislikes: -1 }, $pull: { usersDisliked: req.body.userId } })
             .then((sauce) => res.status(200).json({ message: 'Déselectionné !' }))
             .catch((error) => res.status(400).json({ error }));
